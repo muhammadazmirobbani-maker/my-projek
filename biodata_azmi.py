c9a82a1a9ae8c5403b7aae18b730a61c63c56755
muhammad azmi robbani <muhammadazmirobbani@smkitihsanulfikri.sch.id>
2026-01-28 11:58:54 +0700

    biodata

---
+++ biodata_azmi.py
@@ -0,0 +1,14 @@
+print("=================================")
+print("Program Biodata Sederhana Dengan Python")
+print("=================================")
+
+nama = "azmi robbani"
+hobi = "main bola"
+alamat = "cigelam"
+kota = "purwakarta"
+
+print("Nama Lengkap :", nama)
+print("Alamat       :", alamat, "\n Kota :", kota)
+print("Hobi         :", hobi)
+
+print("=================================")
